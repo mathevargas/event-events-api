33dedd2c6a115e3338c412dac591b55e37978af0
mathevargas <matheusvargask2@gmail.com>
2025-11-01 19:08:35 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/sistema/eventsapi/EventsApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.eventsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/eventsapi/EventsApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.eventsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
